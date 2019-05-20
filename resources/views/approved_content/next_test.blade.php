a315e512ce55e6815273c7440650480b04ac6ed4
nepetadosmil <25409753+nepetadosmil@users.noreply.github.com>
2019-05-20 13:24:34 +0200

    Added next test ending field

--- resources/views/approved_content/next_test.blade.php
+++ resources/views/approved_content/next_test.blade.php
@@ -4,6 +4,7 @@
 	<!--No further dates have been planned yet.<br/>
 	Feel free to mess around with your friends!-->
 	<b>Day:</b> Wednesday, 22nd May<br/>
-	<b>Time:</b> 2300z - ????z<br/>
+	<b>Start:</b> 2300z<br/>
+	<b>Ending:</b> Unknown<br/>
 	<b>Place:</b> <a href="https://minniecenter.org/">ZMP ARTCC</a><br/>
 </div>
