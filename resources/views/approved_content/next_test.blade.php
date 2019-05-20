b1535a2f33ce870fb6198b72d89eb6a5a9cd0b2d
nepetadosmil <25409753+nepetadosmil@users.noreply.github.com>
2019-05-20 13:22:57 +0200

    Added next test date

--- resources/views/approved_content/next_test.blade.php
+++ resources/views/approved_content/next_test.blade.php
@@ -1,9 +1,9 @@
 <div class="alert alert-primary text-left h-full">
 	<h4 class="alert-heading">Upcoming Tests</h4>
 	<hr/>
-	No further dates have been planned yet.<br/>
-	Feel free to mess around with your friends!
-	<!--<b>Day:</b> Someday, Nth Month<br/>
-	<b>Time:</b> XXXXz - XXXXz<br/>
-	<b>Place:</b> Gary's dreams<br/>-->
+	<!--No further dates have been planned yet.<br/>
+	Feel free to mess around with your friends!-->
+	<b>Day:</b> Wednesday, 22nd May<br/>
+	<b>Time:</b> 2300z - ????z<br/>
+	<b>Place:</b> <a href="https://minniecenter.org/">ZMP ARTCC</a><br/>
 </div>
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -7,8 +7,10 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 	<link rel="stylesheet" href="{{ mix('css/custom.css') }}">
+	<!--<link rel="stylesheet" href="/css/app.css">
+	<link rel="stylesheet" href="/css/custom.css">-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans">
-    <title>VATSIM AFV Beta</title>
+    <title>AFV | Audio For VATSIM Beta</title>
 </head>
 <body class="bg-image font-sans">
 
