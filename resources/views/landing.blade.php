e97bf052eecc59b60c601a668beecb3f33f7daa7
nepetadosmil <25409753+nepetadosmil@users.noreply.github.com>
2019-05-23 06:13:21 +0200

    Removed last test date

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-	<link rel="stylesheet" href="{{ mix('css/custom.css') }}">
-	<!--<link rel="stylesheet" href="/css/app.css">
-	<link rel="stylesheet" href="/css/custom.css">-->
+    <link rel="stylesheet" href=" mix('css/app.css') ">
+	<link rel="stylesheet" href=" mix('css/custom.css') ">
+	<link rel="stylesheet" href="/css/app.css">
+	<link rel="stylesheet" href="/css/custom.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans">
     <title>AFV | Audio For VATSIM Beta</title>
 </head>
