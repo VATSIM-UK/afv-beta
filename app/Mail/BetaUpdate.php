fdec4427f8bf0aa8e5f91bc0ff91b19d3efc0cc5
Aidan Stevens <aidan@aidanstevens.ca>
2019-05-27 23:01:59 -0400

    Add beta team application update email

---
+++ app/Mail/BetaUpdate.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class BetaUpdate extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from("noreply@vatsim.net")->subject("Beta Team Application Update")->markdown('emails.beta.update');
+    }
+}
---
+++ resources/views/emails/beta/update.blade.php
@@ -0,0 +1,24 @@
+@component('mail::message')
+# Beta Team Application Update
+
+Dear Audio for VATSIM Beta Applicant,
+
+We wanted to provide you with an update on the beta testing and the tester selection.
+
+We have conducted 3 localized tests on our beta server and have found some small issues each time which have been resolved quickly by our development team.
+
+We have been adding small numbers of applicants to the testing server over the past few weeks and are up to about 200 testers. We will continue to add testers at random over the coming week so don’t count yourself out!
+
+We will need to add a large amount of testers in the next two weeks as we are planning on showcasing the Audio for VATSIM Beta at FlightSim Expo June 8-9th. In order to ensure a consistent amount of traffic for this test we have created slots for arrivals and departures from Orlando International (KMCO).
+
+If you are able to fly during the hours of FlightSim Expo you are welcome to book a slot at vats.im/afv starting June 1st at 1900z. All pilots who receive a slot will be added to the beta!
+
+Thank you for your interest in participating in the Audio for VATSIM beta and we hope to see you on the server very soon!
+
+@component('mail::button', ['url' => 'https://vats.im/afv'])
+Book a Slot
+@endcomponent
+
+Best Regards,<br>
+Audio for VATSIM Team
+@endcomponent
