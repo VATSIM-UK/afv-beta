7d17da9094515201e98a1dbe0d666bbaf1fb9779
nepetadosmil <25409753+nepetadosmil@users.noreply.github.com>
2019-05-31 18:46:10 +0200

    Small styling tweak

--- resources/views/approved_content/top_nav.blade.php
+++ resources/views/approved_content/top_nav.blade.php
@@ -1,21 +1,21 @@
 <!-- NAV TOP -->
 <ul class="bg-blue nav nav-pills nav-justified rounded text-left select-none mb-0" id="pills-tab" role="tablist">
-  <li class="nav-item border">
+  <li class="nav-item border-left">
 	<a class="nav-link text-white active" id="home-tab" data-toggle="pill" href="#home" role="tab" aria-controls="home" aria-selected="false">HOME</a>
   </li>
-  <li class="nav-item border-left border-top border-bottom">
+  <li class="nav-item border-left">
 	<a class="nav-link text-white" id="pilots-tab" data-toggle="pill" href="#pClients" role="tab" aria-controls="pClients" aria-selected="false">PILOTS</a>
   </li>
-  <li class="nav-item border-left border-top border-bottom">
+  <li class="nav-item border-left">
 	<a class="nav-link text-white" id="controllers-tab" data-toggle="pill" href="#cClients" role="tab" aria-controls="cClients" aria-selected="false">CONTROLLERS</a>
   </li>
-  <li class="nav-item border-left border-top border-bottom">
+  <li class="nav-item border-left">
 	<a class="nav-link text-white" id="sups-tab" data-toggle="pill" href="#sups" role="tab" aria-controls="sups" aria-selected="false">SUPERVISORS</a>
   </li>
-  <li class="nav-item border-left border-top border-bottom">
+  <li class="nav-item border-left">
 	<a class="nav-link text-white" id="atis-tab" data-toggle="pill" href="#atis" role="tab" aria-controls="atis" aria-selected="false">ATIS BOTS</a>
   </li>
-  <li class="nav-item border">
+  <li class="nav-item border-left border-right">
 	<a class="nav-link text-white" id="issues-tab" data-toggle="pill" href="#issues" role="tab" aria-controls="issues" aria-selected="false">ISSUES</a>
   </li>
 </ul>
