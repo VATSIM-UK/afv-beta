49baad76790503bb9b5033226a0f6da3946ffd3d
nepetadosmil <25409753+nepetadosmil@users.noreply.github.com>
2019-06-04 14:42:07 +0200

    Updated vPilot version

--- resources/views/approved_content/vPilot.blade.php
+++ resources/views/approved_content/vPilot.blade.php
@@ -2,7 +2,7 @@
     vPilot
 </p>
 <p class="text-grey-darker text-base text-left">
-    To use new voice as a vPilot user is very simple! Download the vPilot Beta from <a href="http://vpilot.metacraft.com/Assets/Files/Installers/AfvBeta/vPilotAFVBeta-Setup-2.2.2.3.exe">Here</a> and run the relevant vPilot (AFV Beta) Shortcuts.
+    To use new voice as a vPilot user is very simple! Download the vPilot Beta from <a href="http://vpilot.metacraft.com/Assets/Files/Installers/AfvBeta/vPilotAFVBeta-Setup-2.2.2.7.exe">Here</a> and run the relevant vPilot (AFV Beta) Shortcuts.
     You may choose any server when connecting, as this client will automatically route you to the AFV-Beta server.<br />
     You DO NOT need the standalone client listed below.<br /><br />
 </p>
