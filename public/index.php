6fe0d41076e5b998ea3a57830dc93dea0a8010f4
Aidan Stevens <aidan@aidanstevens.ca>
2019-06-08 01:26:02 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -47,6 +47,9 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |
 */
 
+// Allow CORS
+header('Access-Control-Allow-Origin: *');
+
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
