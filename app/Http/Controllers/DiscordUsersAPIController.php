8f3cf70dd80cc45d4dbe3330d68e71465aacca23
Aidan Stevens <aidan@aidanstevens.ca>
2019-06-09 10:14:48 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/DiscordUsersAPIController.php
+++ app/Http/Controllers/DiscordUsersAPIController.php
@@ -2,27 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Models\User;
 use App\Models\Discord_Account;
 
 class DiscordUsersAPIController extends Controller
 {
-    public function __invoke(){
+    public function __invoke()
+    {
         $linked = Discord_Account::all();
         $output = [];
 
         foreach ($linked as $link) {
             $user = User::where('id', $link->user_id)->first();
-            $name = $user->full_name . ' - ' . $user->id;
+            $name = $user->full_name.' - '.$user->id;
 
             $approval = $user->approval;
-            if (! $approval) $approved = false;
-            else if ($approval->approved) $approved = true;
-            else $approved = false;
+            if (! $approval) {
+                $approved = false;
+            } elseif ($approval->approved) {
+                $approved = true;
+            } else {
+                $approved = false;
+            }
 
-            $output[$link->id] = ["display_name" => $name, "approved" => $approved];
+            $output[$link->id] = ['display_name' => $name, 'approved' => $approved];
         }
 
         return response($output)->header('Content-Type', 'application/json');
