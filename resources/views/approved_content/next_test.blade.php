fb2cd65123bae43e7e6627c143306afb45146964
nepetadosmil <25409753+nepetadosmil@users.noreply.github.com>
2019-06-09 23:06:53 +0200

    Removed test

--- resources/views/approved_content/next_test.blade.php
+++ resources/views/approved_content/next_test.blade.php
@@ -1,13 +1,13 @@
 <div class="alert alert-primary text-left h-full">
-	<h4 class="alert-heading">Upcoming Tests - FSExpo</h4>
-	<!--<hr/>
+	<h4 class="alert-heading">Upcoming Tests</h4>
+	<hr/>
 	No further dates have been planned yet.<br/>
-	Fill <a href="https://forms.gle/y4aXMWCGm7xxC6cW6">THIS FORM</a> in so we can organize the next one.-->
-	<i><b>Departures:</b> RDU, MSY, ATL & MIA<br/>
+	<!--Fill <a href="https://forms.gle/y4aXMWCGm7xxC6cW6">THIS FORM</a> in so we can organize the next one.-->
+	<!--<i><b>Departures:</b> RDU, MSY, ATL & MIA<br/>
 	<b>Arrivals:</b> MCO<br/></i>
 	<hr />
 	<b>Day:</b> Sunday, 9th June<br/>
 	<b>Start:</b> 1330z<br/>
-	<b>Ending:</b> 1900z<br/>
+	<b>Ending:</b> 1900z<br/>-->
 	<!--<b>Location:</b> Heathrow (EGLL) from/to Oslo-Gardermoen (ENGM)<br/>-->
 </div>
