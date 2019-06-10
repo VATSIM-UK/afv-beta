ab04b16ef611460ce8c04c8cf84a3c204bf692fc
nepetadosmil <25409753+nepetadosmil@users.noreply.github.com>
2019-06-11 00:05:35 +0200

    Redirect to intended on login

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect(route('landing'));
+            return redirect()->intended(route('landing'));
         }
 
         return $next($request);
