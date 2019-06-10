a96efcaf7642225f0cc52cc3d2e3e6f0fb1b1d40
Aidan Stevens <aidan@aidanstevens.ca>
2019-06-10 15:40:05 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/ApprovalController.php
+++ app/Http/Controllers/ApprovalController.php
@@ -14,27 +14,26 @@ class ApprovalController extends Controller
      * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-     public function approve($cid, Request $request)
-     {
-         $approval = Approval::pending()->where('user_id', $cid); // See if user has a pending approval
- 
-         if ($approval == null) { // If user hasn't got a pending approval
+    public function approve($cid, Request $request)
+    {
+        $approval = Approval::pending()->where('user_id', $cid); // See if user has a pending approval
+
+        if ($approval == null) { // If user hasn't got a pending approval
              $approval = Approval::where('user_id', $cid); // See if user has any approval (pending or not)
              if (! $approval) {
                  return redirect()->back()->withError('User not found.')->withApprove('');
              } // User not found
-             else {
-                 return redirect()->back()->withError('User is already approved.')->withApprove('');
-             } // Can't approve an alreay approved user
-         } else {
-             $approval = $approval->first();
-         } // Get the approval
- 
-         $approval->setAsApproved();
- 
-         return redirect()->back()->withSuccess('User successfully approved!')->withApprove('');
-     }
+            else {
+                return redirect()->back()->withError('User is already approved.')->withApprove('');
+            } // Can't approve an alreay approved user
+        } else {
+            $approval = $approval->first();
+        } // Get the approval
 
+        $approval->setAsApproved();
+
+        return redirect()->back()->withSuccess('User successfully approved!')->withApprove('');
+    }
 
     /**
      * Revoke a user's approval.
@@ -72,21 +71,27 @@ class ApprovalController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function random(Request $request)
-    {        
+    {
         $qty = $request->input('qty', 0);
-        if (! $qty) return redirect()->back()->withApprove('');
+        if (! $qty) {
+            return redirect()->back()->withApprove('');
+        }
 
         $pending = Approval::pending()->take($request->input('qty', 0))->inRandomOrder()->get();
 
         // No approvals pending
-        if (! $pending->count()) return redirect()->back()->withError("No pending approvals")->withApprove('');
+        if (! $pending->count()) {
+            return redirect()->back()->withError('No pending approvals')->withApprove('');
+        }
 
         $approved = 0;
         foreach ($pending as $approval) {
-            if (! $approval->user) continue; // If it doesn't belong to any user, it will fail when trying to find who to send mail to
+            if (! $approval->user) {
+                continue;
+            } // If it doesn't belong to any user, it will fail when trying to find who to send mail to
 
             $approval->setAsApproved();
-            ++$approved;
+            $approved++;
         }
 
         return redirect()->back()->withSuccess("Successfully approved $approved users")->withApprove('');
