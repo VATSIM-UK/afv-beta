d1dfb31ecae71b7b3e9ba1d532d2153c1727b4e8
Aidan Stevens <aidan@aidanstevens.ca>
2019-06-13 09:58:21 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,22 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function add(Request $request){
+    public function add(Request $request)
+    {
         $cid = $request->input('id', '');
-        if (strlen($cid) <= 0)
-            return redirect()->back()->withError("User invalid");
+        if (strlen($cid) <= 0) {
+            return redirect()->back()->withError('User invalid');
+        }
 
-        if (! User::where('id', $cid)->exists())
+        if (! User::where('id', $cid)->exists()) {
             return redirect()->back()->withError("User doesn't exist");
+        }
 
         $user = User::where('id', $cid)->first();
         $user->admin = true;
         $user->save();
-        return redirect()->back()->withSuccess("User approved as admin");
+
+        return redirect()->back()->withSuccess('User approved as admin');
     }
 }
