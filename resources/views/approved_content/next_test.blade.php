ce4e4cbe601eced61b7af5fbe3c92460f6116fd0
nepetadosmil <25409753+nepetadosmil@users.noreply.github.com>
2019-06-30 01:14:39 +0200

    Added no tests reason

--- resources/views/approved_content/next_test.blade.php
+++ resources/views/approved_content/next_test.blade.php
@@ -1,7 +1,16 @@
-<div class="alert alert-primary text-left h-full">
-	<h4 class="alert-heading">Upcoming Tests</h4>
-	<hr/>
-	No further dates have been planned yet.<br/>
+<div class="alert alert-primary h-full">
+	<div class="text-left">
+		<p class="my-1">
+			<h4 class="alert-heading">Upcoming Tests</h4>
+			<hr/>
+			No further dates have been planned yet.<br/>
+		</p>
+		<button class="btn btn-primary my-2" type="button" data-toggle="collapse" data-target="#whyNoTest" aria-expanded="false" aria-controls="whyNoTest">Why not?</button>
+	</div>
+	<div class="collapse multi-collapse mt-2" id="whyNoTest">
+		<img src="/images/one_does_not_simply_develop.jpg" class="w-auto" alt="One Does Not Simply 'Develop' the New Codec">
+		<div class="text-xs text-grey-darkest text-right mx-2"><i>Credit: Unya</i></div>
+    </div>
 	<!--Fill <a href="https://forms.gle/y4aXMWCGm7xxC6cW6">THIS FORM</a> in so we can organize the next one.-->
 	<!--<i><b>Departures:</b> RDU, MSY, ATL & MIA<br/>
 	<b>Arrivals:</b> MCO<br/></i>
